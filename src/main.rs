46d1925c871479562c8fe0458a88338245ea1aab
koschasialis <koschasialis@gmail.com>
2026-01-05 22:13:30 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,46 @@
 #![allow(unused_imports)]
 
 use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use std::io::Result;
 
-fn handle_connection(mut stream: TcpStream) -> Result<()> {
+async fn handle_connection(mut stream: TcpStream) -> Result<()> {
+   let mut buf = [0u8; 1024];
    loop {
-       let mut buf = [0u8; 4096];
-       let n = stream.read(&mut buf)?;
-       if n == 0 {
-           return Ok(());
-       }
-       stream.write_all(b"+PONG\r\n")?;
+       let _ = match stream.read(&mut buf).await {
+           Ok(0) => return Ok(()),
+           Ok(n) => {
+               eprintln!("Received data in socket!");
+               n
+           }
+           Err(e) => {
+               eprintln!("failed to read from socket; err = {:?}", e);
+               return Ok(())
+           }
+       };
+
+       eprintln!("Sending pong!");
+
+       stream.write_all(b"+PONG\r\n").await?;
    }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment the code below to pass the first stage
-    //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    loop {
+        eprintln!("Waiting for new client...");
+        let socket = listener.accept().await?.0;
+        eprintln!("Accepted a client!");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_connection(stream).unwrap()
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(socket).await {
+                eprintln!("connection error: {:?}", e);
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
