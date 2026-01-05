a9e549049a391de6353f79722a5c34fea10dcdf4
koschasialis <koschasialis@gmail.com>
2026-01-05 21:16:19 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 #![allow(unused_imports)]
 
-use std::io::Write;
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::io::Result;
+
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
+   loop {
+       let mut buf = [0u8; 4096];
+       let n = stream.read(&mut buf)?;
+       if n == 0 {
+           return Ok(());
+       }
+       stream.write_all(b"+PONG\r\n")?;
+   }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,8 +25,8 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
+            Ok(stream) => {
+                handle_connection(stream).unwrap()
             }
             Err(e) => {
                 println!("error: {}", e);
